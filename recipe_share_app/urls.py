2f28af882d86c30b988cd3522332059979f9b1ca
dutes <dutes@users.noreply.github.com>
2024-12-09 22:32:39 +0000

    Fix: Added debug checks fo JS retrive in editrecipe.html. Fixed typo in urls.py

--- recipe_share_app/urls.py
+++ recipe_share_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('desserts/', views.desserts, name='desserts'), # path for the desserts page
     path('add_recipe/', views.add_recipe, name='add_recipe'), # path for the add recipe page
     path('delete/', views.delete, name='delete'), # path for the delete recipe page
-    path('edit_recipe/', views.edit_recipe, name='edit_recipe'), # path for the edit recipe page
-    path('recipe_details/<int:recipe_id>/', views.recipe_details, name='recipe_details'), # path for the recipe details page
-    path('edit/<int:recipe_id>/', views.edit, name='update_recipe'), # path for the edit recipe page
+    path('edit_recipe/', views.edit_recipe, name='edit_recipe'),  # For listing recipes to edit
+    path('recipe_details/<int:recipe_id>/', views.recipe_details, name='recipe_details'),  # For fetching recipe details
+    path('edit/<int:recipe_id>/', views.edit, name='update_recipe'), # For updating a recipe
 ]
