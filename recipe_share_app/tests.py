49c40b342b544e229ec16c22c4413c0a954f9ff0
dutes <dutes@users.noreply.github.com>
2024-12-14 18:08:50 +0000

    Add: unit test, cleaned up footer on base.html

--- recipe_share_app/tests.py
+++ recipe_share_app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from recipe_share_app.models import Recipe
+
+class RecipeModelTest(TestCase):
+    def test_recipe_creation(self):
+        recipe=Recipe.objects.create(
+            name="Test Recipe",
+            ingredients="1 cup sugar, 2 cups of flour",
+            instructions="Bang it all in a bowl and mix",
+            category="Desserts"
+    )
+    self.assertEqual(recipe.name, "Test Recipe")
+    self.assertEqual(recipe.category, "Desserts")
+    self.assertsIsNotNone(recipe.date_created)
